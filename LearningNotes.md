ebe72cd5e3c8d3aec84d610cf6bd00546428e282
Yuxin Hu <yuxinhu@google.com>
2024-04-27 22:31:08 -0700

    Add a simple compute shader to update array data

---
+++ LearningNotes.md
@@ -0,0 +1,3 @@
+## April 27th 2024
+
+VkDevice must be destroyed before VkInstance.
--- VulkanSynchronizationLearningVulkanCompute/main.cpp
+++ VulkanSynchronizationLearningVulkanCompute/main.cpp
@@ -3,11 +3,16 @@
 #include <iostream>
 #include <vector>
 #include <optional>
+#include <array>
+#include <fstream>
+#include <cassert>
 
 
 const std::vector<const char*> validationLayers = {
 	"VK_LAYER_KHRONOS_validation"
 };
+
+const uint32_t DATA_BUFFER_SIZE = 256;
 
 #ifdef NDEBUG
 const bool enableValidationLayers = false;
@@ -39,6 +44,24 @@ public:
 		initVulkan();
 		mainLoop();
 		cleanup();
+	}
+
+	static std::vector<char> readFile(const std::string& filename) {
+		std::cout << "Yuxin Debug filename is: " << filename << std::endl;
+		std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+		if (!file.is_open()) {
+			throw std::runtime_error("failed to open file!");
+		}
+
+		size_t fileSize = (size_t)file.tellg();
+		std::vector<char> buffer(fileSize);
+
+		file.seekg(0);
+		file.read(buffer.data(), fileSize);
+		file.close();
+		return buffer;
+
 	}
 
 private:
@@ -228,12 +251,285 @@ private:
 			createInfo.enabledLayerCount = 0;
 		}
 
-		if (vkCreateDevice(mPhysicalDevice, &createInfo, nullptr, &mDevice) != VK_SUCCESS) {
-			throw std::runtime_error("failed to create logical device!");
+if (vkCreateDevice(mPhysicalDevice, &createInfo, nullptr, &mDevice) != VK_SUCCESS) {
+	throw std::runtime_error("failed to create logical device!");
+}
+
+vkGetDeviceQueue(mDevice, indices.computeFamily.value(), 0, &mComputeQueue);
+
+	}
+
+	void createComputeDescriptorSetLayout() {
+		std::array<VkDescriptorSetLayoutBinding, 2> layoutBindings{};
+		layoutBindings[0].binding = 0;
+		layoutBindings[0].descriptorCount = 1;
+		layoutBindings[0].descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+		layoutBindings[0].pImmutableSamplers = nullptr;
+		layoutBindings[0].stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
+
+		layoutBindings[1].binding = 1;
+		layoutBindings[1].descriptorCount = 1;
+		layoutBindings[1].descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+		layoutBindings[1].pImmutableSamplers = nullptr;
+		layoutBindings[1].stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
+
+		VkDescriptorSetLayoutCreateInfo layoutInfo{};
+		layoutInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+		layoutInfo.bindingCount = 2;
+		layoutInfo.pBindings = layoutBindings.data();
+
+		if (vkCreateDescriptorSetLayout(mDevice, &layoutInfo, nullptr, &mComputeDescriptorSetLayout) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create compute descriptor set layout!");
+		}
+	}
+
+	VkShaderModule createShaderModule(const std::vector<char>& code) {
+		VkShaderModuleCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+		createInfo.codeSize = code.size();
+		createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+
+		VkShaderModule shaderModule;
+		if (vkCreateShaderModule(mDevice, &createInfo, nullptr, &shaderModule) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create shader module!");
 		}
 
-		vkGetDeviceQueue(mDevice, indices.computeFamily.value(), 0, &mComputeQueue);
+		return shaderModule;
+	}
 
+	void createComputePipeline() {
+		// TODO change the spirv shader path
+		const std::string computShaderFileName = "VulkanSynchronizationLearningVulkanCompute\\simpleBufferUpdate.spv";
+
+		auto computeShaderCode = readFile(SOLUTION_DIR + computShaderFileName);
+
+		VkShaderModule computeShaderModule = createShaderModule(computeShaderCode);
+
+		VkPipelineShaderStageCreateInfo computeShaderStageInfo{};
+		computeShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+		computeShaderStageInfo.stage = VK_SHADER_STAGE_COMPUTE_BIT;
+		computeShaderStageInfo.module = computeShaderModule;
+		computeShaderStageInfo.pName = "main";
+
+		VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
+		pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+		pipelineLayoutInfo.setLayoutCount = 1;
+		pipelineLayoutInfo.pSetLayouts = &mComputeDescriptorSetLayout;
+
+		if (vkCreatePipelineLayout(mDevice, &pipelineLayoutInfo, nullptr, &mComputePipelineLayout) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create compute pipeline layout!");
+		}
+
+		VkComputePipelineCreateInfo pipelineInfo{};
+		pipelineInfo.sType = VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO;
+		pipelineInfo.layout = mComputePipelineLayout;
+		pipelineInfo.stage = computeShaderStageInfo;
+
+		if (vkCreateComputePipelines(mDevice, VK_NULL_HANDLE, 1, &pipelineInfo, nullptr, &mComputePipeline) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create compute pipeline!");
+		}
+
+		vkDestroyShaderModule(mDevice, computeShaderModule, nullptr);
+	}
+
+	void createCommandPool() {
+		QueueFamilyIndices queueFamilyIndices = findQueueFamilies(mPhysicalDevice);
+
+		VkCommandPoolCreateInfo poolInfo{};
+		poolInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+		poolInfo.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
+		poolInfo.queueFamilyIndex = queueFamilyIndices.computeFamily.value();
+
+		if (vkCreateCommandPool(mDevice, &poolInfo, nullptr, &mCommandPool) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create compute command pool!");
+		}
+	}
+
+	uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties) {
+		VkPhysicalDeviceMemoryProperties memProperties;
+		vkGetPhysicalDeviceMemoryProperties(mPhysicalDevice, &memProperties);
+
+		for( uint32_t i = 0; i < memProperties.memoryTypeCount; i++) {
+			if ((typeFilter & (1 << i)) && (memProperties.memoryTypes[i].propertyFlags & properties) == properties) {
+				return i;
+			}
+		}
+
+		throw std::runtime_error("failed to find suitable memory type!");
+	}
+
+	void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer, VkDeviceMemory& bufferMemory) {
+		VkBufferCreateInfo bufferInfo{};
+		bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+		bufferInfo.size = size;
+		bufferInfo.usage = usage;
+		bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+		if (vkCreateBuffer(mDevice, &bufferInfo, nullptr, &buffer) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create buffer!");
+		}
+
+		VkMemoryRequirements memRequirements;
+		vkGetBufferMemoryRequirements(mDevice, buffer, &memRequirements);
+
+		VkMemoryAllocateInfo allocInfo{};
+		allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+		allocInfo.allocationSize = memRequirements.size;
+		allocInfo.memoryTypeIndex = findMemoryType(memRequirements.memoryTypeBits, properties);
+
+		if (vkAllocateMemory(mDevice, &allocInfo, nullptr, &bufferMemory) != VK_SUCCESS) {
+			throw std::runtime_error("failed to allocate buffer memory!");
+		}
+
+		vkBindBufferMemory(mDevice, buffer, bufferMemory, 0);
+	}
+
+	void copyBuffer(VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size) {
+		VkCommandBufferAllocateInfo allocInfo{};
+		allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+		allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+		allocInfo.commandPool = mCommandPool;
+		allocInfo.commandBufferCount = 1;
+
+		VkCommandBuffer commandBuffer;
+		vkAllocateCommandBuffers(mDevice, &allocInfo, &commandBuffer);
+
+		VkCommandBufferBeginInfo beginInfo{};
+		beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+		beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+
+		vkBeginCommandBuffer(commandBuffer, &beginInfo);
+
+		VkBufferCopy copyRegion{};
+		copyRegion.size = size;
+		vkCmdCopyBuffer(commandBuffer, srcBuffer, dstBuffer, 1, &copyRegion);
+		vkEndCommandBuffer(commandBuffer);
+
+		VkSubmitInfo submitInfo{};
+		submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+		submitInfo.commandBufferCount = 1;
+		submitInfo.pCommandBuffers = &commandBuffer;
+
+		vkQueueSubmit(mComputeQueue, 1, &submitInfo, VK_NULL_HANDLE);
+		vkQueueWaitIdle(mComputeQueue);
+
+		vkFreeCommandBuffers(mDevice, mCommandPool, 1, &commandBuffer);
+	}
+
+	void createShaderStorageBuffer() {
+		std::vector<int> computeShaderInputData(DATA_BUFFER_SIZE);
+		for (int inputDataIndex = 0; inputDataIndex < DATA_BUFFER_SIZE; ++inputDataIndex) {
+			computeShaderInputData[inputDataIndex] = inputDataIndex;
+		}
+
+		VkDeviceSize bufferSize = sizeof(int) * DATA_BUFFER_SIZE;
+
+		// staging buffer used to upload data to gpu
+		VkBuffer stagingBuffer;
+		VkDeviceMemory stagingBufferMemory;
+		createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+			stagingBuffer, stagingBufferMemory);
+
+		void* data;
+		vkMapMemory(mDevice, stagingBufferMemory, 0, bufferSize, 0, &data);
+		memcpy(data, computeShaderInputData.data(), static_cast<size_t>(bufferSize));
+		vkUnmapMemory(mDevice, stagingBufferMemory);
+
+		createBuffer(bufferSize, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, mShaderStorageBuffer, mShaderStorageBufferMemory);
+		copyBuffer(stagingBuffer, mShaderStorageBuffer, bufferSize);
+
+		vkDestroyBuffer(mDevice, stagingBuffer, nullptr);
+		vkFreeMemory(mDevice, stagingBufferMemory, nullptr);
+	}
+
+	void createDescriptorPool() {
+		std::array<VkDescriptorPoolSize, 1> poolSizes{};
+		poolSizes[0].type = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+		poolSizes[0].descriptorCount = 2;
+
+		VkDescriptorPoolCreateInfo poolInfo{};
+		poolInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
+		poolInfo.poolSizeCount = 1;
+		poolInfo.pPoolSizes = poolSizes.data();
+		poolInfo.maxSets = static_cast<uint32_t>(1);
+
+		if (vkCreateDescriptorPool(mDevice, &poolInfo, nullptr, &mDescriptorPool) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create descriptor pool!");
+		}
+	}
+
+	void createComputeDescriptorSets() {
+		std::vector<VkDescriptorSetLayout> layouts(1, mComputeDescriptorSetLayout);
+		VkDescriptorSetAllocateInfo allocInfo{};
+		allocInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
+		allocInfo.descriptorPool = mDescriptorPool;
+		allocInfo.descriptorSetCount = static_cast<uint32_t>(1);
+		allocInfo.pSetLayouts = layouts.data();
+
+		if (vkAllocateDescriptorSets(mDevice, &allocInfo, &mComputeDescriptorSet) != VK_SUCCESS) {
+			throw std::runtime_error("failed to allocate descriptor sets!");
+		}
+
+		VkDescriptorBufferInfo storageBufferInfo{};
+		storageBufferInfo.buffer = mShaderStorageBuffer;
+		storageBufferInfo.offset = 0;
+		storageBufferInfo.range = sizeof(int) * DATA_BUFFER_SIZE;
+
+		std::array<VkWriteDescriptorSet, 2> descriptorWrites{};
+		descriptorWrites[0].sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+		descriptorWrites[0].dstSet = mComputeDescriptorSet;
+		descriptorWrites[0].dstBinding = 0;
+		descriptorWrites[0].dstArrayElement = 0;
+		descriptorWrites[0].descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+		descriptorWrites[0].descriptorCount = 1;
+		descriptorWrites[0].pBufferInfo = &storageBufferInfo;
+
+		descriptorWrites[1].sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+		descriptorWrites[1].dstSet = mComputeDescriptorSet;
+		descriptorWrites[1].dstBinding = 1;
+		descriptorWrites[1].dstArrayElement = 0;
+		descriptorWrites[1].descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+		descriptorWrites[1].descriptorCount = 1;
+		descriptorWrites[1].pBufferInfo = &storageBufferInfo;
+
+		vkUpdateDescriptorSets(mDevice, 2, descriptorWrites.data(), 0, nullptr);
+	}
+
+	void createComputeCommandBuffers() {
+		VkCommandBufferAllocateInfo allocInfo{};
+		allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+		allocInfo.commandPool = mCommandPool;
+		allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+		allocInfo.commandBufferCount = 1;
+		if (vkAllocateCommandBuffers(mDevice, &allocInfo, &mComputeCommandBuffers) != VK_SUCCESS) {
+			throw std::runtime_error("failed to allocate compute command buffers!");
+		}
+	}
+
+	void createSyncObjects() {
+		VkFenceCreateInfo fenceInfo{};
+		fenceInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+
+		if (vkCreateFence(mDevice, &fenceInfo, nullptr, &mComputeInFlightFence) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create compute synchronization object");
+		}
+	}
+
+	void recordComputeCommandBuffer(VkCommandBuffer commandBuffer) {
+		VkCommandBufferBeginInfo beginInfo{};
+		beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+
+		if (vkBeginCommandBuffer(commandBuffer, &beginInfo) != VK_SUCCESS) {
+			throw std::runtime_error("failed to begin recording compute command buffer!");
+		}
+
+		vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_COMPUTE, mComputePipeline);
+		vkCmdBindDescriptorSets(commandBuffer, VK_PIPELINE_BIND_POINT_COMPUTE, mComputePipelineLayout, 0, 1, &mComputeDescriptorSet, 0, nullptr);
+		vkCmdDispatch(commandBuffer, DATA_BUFFER_SIZE / 256, 1, 1);
+
+		if (vkEndCommandBuffer(commandBuffer) != VK_SUCCESS) {
+			throw std::runtime_error("failed to record compute command buffer!");
+		}
 	}
 
 	void initVulkan() {
@@ -241,17 +537,88 @@ private:
 		setupDebugMessenger();
 		pickPhysicalDevice();
 		createLogicalDevice();
+		createComputeDescriptorSetLayout();
+		createComputePipeline();
+		createCommandPool();
+		createShaderStorageBuffer();
+		createDescriptorPool();
+		createComputeDescriptorSets();
+		createComputeCommandBuffers();
+		createSyncObjects();
 	}
 
 	void mainLoop() {
+		VkSubmitInfo submitInfo{};
+		submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+
+		VkBuffer stagingBuffer;
+		VkDeviceMemory stagingBufferMemory;
+		VkDeviceSize bufferSize = sizeof(int) * DATA_BUFFER_SIZE;
+		createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+			stagingBuffer, stagingBufferMemory);
+		copyBuffer(mShaderStorageBuffer, stagingBuffer, bufferSize);
+		void* stagingBufferMappedData;
+		std::vector<int> computeShaderInputData(DATA_BUFFER_SIZE);
+		vkMapMemory(mDevice, stagingBufferMemory, 0, bufferSize, 0, &stagingBufferMappedData);
+		memcpy(computeShaderInputData.data(), stagingBufferMappedData, static_cast<size_t>(bufferSize));
+		vkUnmapMemory(mDevice, stagingBufferMemory);
+		for (int index = 0; index < DATA_BUFFER_SIZE; ++index) {
+			//assert(computeShaderOutputData[index] == index+1);
+			std::cout << "compute shader input data at index " << index << " is: " << computeShaderInputData[index] << std::endl;
+		}
+
+		// Submit compute commands
+		vkResetCommandBuffer(mComputeCommandBuffers, 0);
+		recordComputeCommandBuffer(mComputeCommandBuffers);
+		submitInfo.commandBufferCount = 1;
+		submitInfo.pCommandBuffers = &mComputeCommandBuffers;
+		submitInfo.signalSemaphoreCount = 0;
+		submitInfo.pSignalSemaphores = nullptr;
+		if (vkQueueSubmit(mComputeQueue, 1, &submitInfo, mComputeInFlightFence) != VK_SUCCESS) {
+			throw std::runtime_error("failed to submit compute command buffer!");
+		}
+
+		// Wait for the compute command finish execution
+		vkWaitForFences(mDevice, 1, &mComputeInFlightFence, VK_TRUE, UINT64_MAX);
+
+		// Read data back from Shader Storage Buffer Objects
+		// staging buffer used to upload data to gpu
+		copyBuffer(mShaderStorageBuffer, stagingBuffer, bufferSize);
+		std::vector<int> computeShaderOutputData(DATA_BUFFER_SIZE);
+		vkMapMemory(mDevice, stagingBufferMemory, 0, bufferSize, 0, &stagingBufferMappedData);
+		memcpy(computeShaderOutputData.data(), stagingBufferMappedData, static_cast<size_t>(bufferSize));
+		vkUnmapMemory(mDevice, stagingBufferMemory);
+		for (int index = 0; index < DATA_BUFFER_SIZE; ++index) {
+			assert(computeShaderOutputData[index] == index+1000);
+			std::cout << "compute shader output data at index " << index << " is: " << computeShaderOutputData[index] << std::endl;
+		}
+
+		vkDestroyBuffer(mDevice, stagingBuffer, nullptr);
+		vkFreeMemory(mDevice, stagingBufferMemory, nullptr);
 
 	}
 
 	void cleanup() {
+		vkDestroyPipeline(mDevice, mComputePipeline, nullptr);
+		vkDestroyPipelineLayout(mDevice, mComputePipelineLayout, nullptr);
+
+		vkDestroyDescriptorPool(mDevice, mDescriptorPool, nullptr);
+		vkDestroyDescriptorSetLayout(mDevice, mComputeDescriptorSetLayout, nullptr);
+
+		vkDestroyBuffer(mDevice, mShaderStorageBuffer, nullptr);
+		vkFreeMemory(mDevice, mShaderStorageBufferMemory, nullptr);
+
+		vkDestroyFence(mDevice, mComputeInFlightFence, nullptr);
+
+		vkDestroyCommandPool(mDevice, mCommandPool, nullptr);
+
+
+		vkDestroyDevice(mDevice, nullptr);
+
 		if (enableValidationLayers) {
 			DestroyDebugUtilsMessengerEXT(mInstance, mDebugMessenger, nullptr);
 		}
-		vkDestroyDevice(mDevice, nullptr);
+
 		vkDestroyInstance(mInstance, nullptr);
 	}
 
@@ -260,6 +627,21 @@ private:
 	VkPhysicalDevice mPhysicalDevice = VK_NULL_HANDLE;
 	VkDevice mDevice;
 	VkQueue mComputeQueue;
+		
+	VkPipelineLayout mComputePipelineLayout;
+	VkPipeline mComputePipeline;
+	VkDescriptorPool mDescriptorPool;
+	VkDescriptorSetLayout mComputeDescriptorSetLayout;
+	VkDescriptorSet mComputeDescriptorSet;
+
+	VkCommandPool mCommandPool;
+
+	VkBuffer mShaderStorageBuffer;
+	VkDeviceMemory mShaderStorageBufferMemory;
+	VkCommandBuffer mComputeCommandBuffers;
+
+	VkFence mComputeInFlightFence;
+
 };
 
 
